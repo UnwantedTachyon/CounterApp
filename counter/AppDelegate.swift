ab33de1ecb3f2625c25c96e9c4989af7e3c66e4d
Harshdeep Singh <harsheep.chhara@icloud.com>
2020-03-24 15:35:19 +0530

    Counter App

--- counter/AppDelegate.swift
+++ counter/AppDelegate.swift
@@ -11,7 +11,14 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var enableAllOrientation = false
 
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if (enableAllOrientation == true){
+            return UIInterfaceOrientationMask.allButUpsideDown
+        }
+        return UIInterfaceOrientationMask.portrait
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- counter/ContentView.swift
+++ counter/ContentView.swift
@@ -9,10 +9,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number:Int = 0
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading, spacing: 200)
+        {
+            Text("Counter: \(number)").font(.system(size: 40)).position(x: 205, y: 200)
+            Button(action: {
+                self.number += 1})
+            {
+                HStack {
+                
+                    Image("next").resizable()
+                        .frame(width: 40, height: 40)
+                        .font(.title)
+                    Text("Count")
+                        .fontWeight(.bold)
+                        .font(.title)
+                }
+                .frame(minWidth: 0, maxWidth: 175, minHeight: 0, maxHeight: 30)
+                .padding()
+                .background(Color.purple)
+                .cornerRadius(40)
+                .foregroundColor(.white)
+                
+                
+            }.position(x: 205, y: 80)
+            
+            
+            Button(action: {
+                self.number = 0})
+            {
+                HStack {
+                    Image("refresh").resizable()
+                        .frame(width: 28.0, height: 28.0)
+                        .font(.title)
+                    Text("Reset").font(.system(size: 20))
+                        .fontWeight(.semibold)
+                        .font(.title)
+                      
+                }
+                .frame(minWidth: 0, maxWidth: 100, minHeight: 0, maxHeight: 20)
+                .padding()
+                .background(Color.red)
+                .cornerRadius(20)
+                .foregroundColor(.white)
+                    
+                }.position(x: 205, y: 20)
+            }
+            
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
